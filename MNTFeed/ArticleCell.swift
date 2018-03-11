5b4d48558dcd4429694ffb00a68e717959479f5e
Cons Bulaquena <consbulaquena@gmail.com>
2018-03-12 05:54:20 +0800

    Try

--- MNTFeed/ArticleCell.swift
+++ MNTFeed/ArticleCell.swift
@@ -16,7 +16,7 @@ class ArticleCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        // Initialization code, updated with git
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
