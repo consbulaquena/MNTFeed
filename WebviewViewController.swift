797fd73f9d3afbf9709dca827d52447e5b86600d
Cons Bulaquena <consbulaquena@gmail.com>
2018-03-12 22:21:25 +0800

    Added Webview

---
+++ WebviewViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WebviewViewController.swift
+//  MNTFeed
+//
+//  Created by Cons Bulaqueña on 12/03/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+
+class WebviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
