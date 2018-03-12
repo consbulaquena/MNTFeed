527805cf598fdf675575c0ce95fffaaf7c52fd22
Cons Bulaquena <consbulaquena@gmail.com>
2018-03-12 20:17:34 +0800

    Fixed TableView, fixed urlRequest,Updated cells

--- MNTFeed/ViewController.swift
+++ MNTFeed/ViewController.swift
@@ -13,19 +13,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var articles: [Article]? = []
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //function will fetch all articles when view did load
-        fetcharticles()
-
+        fetchArticles()
     }
-
-    func fetcharticles(){
-        //1 Create a URL request, url is a string from MNT api
-        let urlRequest = URLRequest(url: URL(string:
-            "https://newsapi.org/v2/top-headlines?sources=medical-news-today&apiKey=c8e6352d8a1840dbb7370b5f159f8f4b")!)
+    
+    func fetchArticles(){
+        let urlRequest = URLRequest(url: URL(string: "https://newsapi.org/v2/top-headlines?sources=medical-news-today&apiKey=c8e6352d8a1840dbb7370b5f159f8f4b")!)
         
         //This will give data = JSon, response = ok? error =error
         let task = URLSession.shared.dataTask(with: urlRequest) { (data,response,error) in
@@ -35,35 +31,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 return
             }
             
-            //empty articles array
             self.articles = [Article]()
-            
-            
             
             //JSON serialization - do try catch block, download JSON, data is URL request
             //String to anyobject - give JSON in format of dictionary
-            
             do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
-               //articles is an array of dictionary, get article
-                if let articlesFromJson = ["articles"] as? [[String: AnyObject]] {// receive value from dictionary
-                //Go each article and extract - for loop
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                
+                if let articlesFromJson = json["articles"] as? [[String : AnyObject]] {
                     for articleFromJson in articlesFromJson {
-                       let article = Article()
-                        
-  
+                        let article = Article()
                         if let title = articleFromJson["title"] as? String, let desc = articleFromJson["description"] as? String, let url = articleFromJson["url"] as? String, let urlToImage = articleFromJson["urlToImage"] as? String {
                             
-
-                        article.headline = title
-                        article.desc = desc
-                        article.url = url
-                        article.imageUrl = urlToImage
+                           
+                            article.desc = desc
+                            article.headline = title
+                            article.url = url
+                            article.imageUrl = urlToImage
                         }
-                        
-                        
+                        self.articles?.append(article)
                     }
-            
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
                 }
                 
             } catch let error {
@@ -73,12 +63,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
         }
         
+        task.resume()
         
     }
     
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath) as! ArticleCell
+        
+        cell.title.text = self.articles?[indexPath.item].headline
+        cell.desc.text = self.articles?[indexPath.item].desc
+
+        cell.imgView.downloadImage(from: (self.articles?[indexPath.item].imageUrl!)!)
         
         return cell
     }
@@ -88,8 +83,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.articles?.count ?? 0
     }
     
 }
 
+extension UIImageView {
+    
+    func downloadImage(from url: String){
+        
+        let urlRequest = URLRequest(url: URL(string: url)!)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data,response,error) in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.image = UIImage(data: data!)
+            }
+        }
+        task.resume()
+    }
+}
